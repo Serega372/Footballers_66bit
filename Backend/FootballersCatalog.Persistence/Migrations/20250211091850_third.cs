859187cc9b98e21da364b35d05d32f2bca7f92d1
Serega <sergey.123321123@mail.ru>
2025-02-11 16:05:55 +0500

    apply migrations

---
+++ Backend/FootballersCatalog.Persistence/Migrations/20250211091850_third.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballersCatalog.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TeamId",
+                table: "Footballers",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TeamId",
+                table: "Footballers",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Backend/FootballersCatalog.Persistence/Migrations/20250211093531_offCascadeDel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballersCatalog.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class offCascadeDel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Footballers_Teams_TeamId",
+                table: "Footballers",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+    }
+}
